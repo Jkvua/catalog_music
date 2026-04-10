aad662167a5c51814dcec2715a95bf8c9d1bde4f
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2026-04-10 19:20:50 -0300

    removendo arquivos da main

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.usuario import Usuario
-from app.models.album import Album
-from app.models.artista import Artista
-from app.models.avaliacao import Avaliacao
-from app.models.musica import Musica
