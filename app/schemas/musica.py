9cd2c7a71dda604000578adf6cbb271a93405442
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2026-04-18 15:25:06 -0300

    feat: add validate_deracao with schemas musica

--- app/schemas/musica.py
+++ app/schemas/musica.py
@@ -3,7 +3,6 @@ from app.models.musica import Musica
 from app.schemas.album import AlbumSchema
 from app.schemas.artista import ArtistaSchema
 from marshmallow import fields, validate
-
 class MusicaInputSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Musica
@@ -13,6 +12,17 @@ class MusicaInputSchema(ma.SQLAlchemyAutoSchema):
         
     titulo = fields.String(required=True, validate=validate.Length(min=1, max=200))
     duracao = fields.String(required=True, validate=validate.Length(min=1, max=30))
+
+    def validate_duracao(self, value):
+        try:
+            minutos, segundos = value.split(':')
+            minutos = int(minutos)
+            segundos = int(segundos)
+
+            if minutos < 0 or segundos < 0 or segundos >= 60:
+                raise validate.ValidationError("Formato de duração da música inválido. Use MM:SS (ex.: 03:45)")
+        except (ValueError, AttributeError):
+            raise validate.ValidationError("A duração da música está em um formato inválido, use MM:SS")
 
 musica_schema = MusicaInputSchema()
 musicas_schema = MusicaInputSchema(many=True)
